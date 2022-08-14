3ebd38547b47d07b1480901c9a5e4fb60f19710c
Denis <denisgreshnaykov@gmail.com>
2022-08-14 18:12:57 +0500

    add accordion

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,41 @@
+const accordion = (triggersSelector) => {
+  const btns = document.querySelectorAll(triggersSelector);
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      btns.forEach((item) => {
+        item.classList.remove("active-style");
+        item.nextElementSibling.classList.remove("active-content");
+        item.nextElementSibling.style.maxHeight = "0px";
+      });
+
+      this.classList.toggle("active-style");
+      this.nextElementSibling.classList.toggle("active-content");
+
+      if (this.classList.contains("active-style")) {
+        this.nextElementSibling.style.maxHeight =
+          this.nextElementSibling.scrollHeight + 80 + "px";
+      } else {
+        this.nextElementSibling.style.maxHeight = "0px";
+      }
+    });
+  });
+  //const blocks = document.querySelectorAll(itemsSelector);
+
+  // blocks.forEach((block) => {
+  //   block.classList.add("animated", "fadeInDown");
+  // });
+
+  // btns.forEach((btn) => {
+  //   btn.addEventListener("click", function () {
+  //     if (!this.classList.contains("active")) {
+  //       btns.forEach((btn) => {
+  //         btn.classList.remove("active", "active-style");
+  //       });
+  //       this.classList.add("active", "active-style");
+  //     }
+  //   });
+  // });
+};
+
+export default accordion;
